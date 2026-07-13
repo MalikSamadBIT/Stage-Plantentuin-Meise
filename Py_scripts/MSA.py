3b0ae14ca42eb9916166bd5979f9abb48106cbe6
MalikSamadBIT <malik.samad@student.howest.be>
2026-07-13 08:20:02 +0200

    removed the test code

--- Py_scripts/MSA.py
+++ Py_scripts/MSA.py
@@ -6,39 +6,6 @@ import customtkinter
 from tkinter import filedialog
 from PIL import Image
 
-'''
-
-Fasta_file = r"B:\Stage\NCBI_5results\Huperzia selago\matK\matK.fasta"
-Aligned_file = r"B:\Stage\NCBI_5results\Huperzia selago\matK\matK_aligned.fasta"
-
-subprocess.run([r"B:\Stage\tools\muscle.exe", "-align", Fasta_file,
-               "-output", Aligned_file], check=True)
-alignment = AlignIO.read(Aligned_file, "fasta")
-
-num_seq = len(alignment)
-print(num_seq)
-alignment_length = alignment.get_alignment_length()
-print(alignment_length)
-
-for record in alignment:
-    print(record)
-    print("-"*60)
-
-mv = MsaViz(Aligned_file, color_scheme="Clustal",
-            show_grid=True, show_count=True, show_consensus=True)
-mv.savefig(r"B:\Stage\NCBI_5results\Huperzia selago\matK\msa_report.png")
-
-
-# Image----------------------------------------------------------------------------------
-
-MSA_img = r"B:\Stage\NCBI_5results\Huperzia selago\matK\msa_report.png"
-
-img = Image.open(MSA_img)
-x, y = img.size
-# print(x, y)
-
-'''
-
 
 def run_MSA(input_file, output_dir):
 
