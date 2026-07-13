f2503e5f2bdbc81b77197ae7c2343638c6dfd61e
MalikSamadBIT <malik.samad@student.howest.be>
2026-07-13 11:13:31 +0200

    When barplot is selected in gui barplot  gets made

--- Py_scripts/Retrieval_rate.py
+++ Py_scripts/Retrieval_rate.py
@@ -1,29 +1,54 @@
+import tkinter
+import customtkinter
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
 df_plot = pd.read_csv(r"B:\Stage\test\summary.csv")
 
-fix, ax = plt.subplots()
 
-markers = ["ITS1_count",  "ITS2_count",  "rbcL_count",
-           "matK_count",  "trnL_count",  "psbA-trnH_count"]
+def Plots(selected_plot):
+    plot_options.configure()
+    if selected_plot == "Barplot":
 
-counts = []
+        fix, ax = plt.subplots()
 
-for marker in markers:
-    m = df_plot[marker].sum()
-    counts.append(m)
+        markers = ["ITS1_count",  "ITS2_count",  "rbcL_count",
+                   "matK_count",  "trnL_count",  "psbA-trnH_count"]
 
-bar_colors = ['red', 'blue', 'green',
-              'orange', 'yellow', 'cyan']
+        counts = []
 
-ax.bar(markers, counts, color=bar_colors)
+        for marker in markers:
+            m = df_plot[marker].sum()
+            counts.append(m)
 
-ax.set_ylabel("Nr of FASTA sequences")
-ax.set_title("FASTA Retrieval Rate")
+        bar_colors = ['red', 'blue', 'green',
+                      'orange', 'yellow', 'cyan']
 
-for index, value in enumerate(counts):
-    ax.text(index, value, str(value), ha="center", va="bottom")
+        ax.bar(markers, counts, color=bar_colors)
 
-plt.show()
+        ax.set_ylabel("Nr of FASTA sequences")
+        ax.set_title("FASTA Retrieval Rate")
+
+        for index, value in enumerate(counts):
+            ax.text(index, value, str(value), ha="center", va="bottom")
+
+        plt.show()
+
+# GUI--------------------------------------------------------------------
+
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+tk = customtkinter.CTk()
+
+tk.title("plot")
+tk.geometry("700x450")
+
+plots = ["Barplot", "piechart", "Table"]
+
+plot_options = customtkinter.CTkOptionMenu(tk, values=plots, command=Plots)
+plot_options.pack(pady=40)
+
+tk.mainloop()
