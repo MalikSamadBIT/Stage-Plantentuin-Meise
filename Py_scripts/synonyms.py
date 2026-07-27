4437edb2e6f77057ddb325b90b4055851f9e7d82
MalikSamadBIT <malik.samad@student.howest.be>
2026-07-27 10:20:11 +0200

    Script for scraping the species page from waarnemingen.be

---
+++ Py_scripts/synonyms.py
@@ -0,0 +1,23 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+test_list = ["Huperzia selago", "Lycopodiella inundata", "Lycopodium clavatum"]
+
+url = "https://waarnemingen.be/search/?q="
+
+for name in test_list:
+    name_split = name.split()
+
+    genus = name_split[0]
+    species = name_split[1]
+    # print(genus + "-" + species)
+    name_url = url + genus + "+" + species
+    # print(name_url)
+
+    page = urlopen(name_url)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+
+    name_page = soup.find_all('a', {'href': re.compile(r'^/species/\d+/$')})
+    print(name_page)
